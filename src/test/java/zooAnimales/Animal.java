2a181d6e9ec6101fa45a0f071bc05b6f8b74bf95
Dajovih <80296654+Dajovih@users.noreply.github.com>
2021-11-23 19:42:05 -0500

    Dios mío

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -57,7 +57,7 @@ public class Animal {
 	public String movimiento() {
 		return "desplazarse";}
 	
-	public String totalPorTipo() {
+	public static String totalPorTipo() {
 		return "Mamiferos: "+Mamifero.cantidadMamiferos()+"\nAves: "+Ave.cantidadAves()+
 		"\nReptiles: "+Reptil.cantidadReptiles()+"\nPeces: "+Pez.cantidadPeces()+"\nAnfibios: "+
 		Anfibio.cantidadAnfibios();}
